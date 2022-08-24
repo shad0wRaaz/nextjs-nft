6b21c4c1c7a9b2580a267c7f81635d6198900a2d
shad0wRaaz <007raaz.k@gmail.com>
2022-08-24 21:46:45 +0100

    Fix: Environment Variables implemented for access keys and secrets

--- express/server.js
+++ express/server.js
@@ -20,18 +20,20 @@ app.use(
 
 dotenv.config()
 
+
+
 const randomImageName = (bytes = 32) =>
   crypto.randomBytes(bytes).toString('hex')
 
-const s3BucketName = 'nuvanft'
-const s3BucketRegion = 'eu-west-2'
-const s3AccessKey = 'AKIA4B5XZ7FBATGSZFK6'
-const s3SecretKey = '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'
+// const s3BucketName = 'nuvanft'
+// const s3BucketRegion = 'eu-west-2'
+// const s3AccessKey = 'AKIA4B5XZ7FBATGSZFK6'
+// const s3SecretKey = '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'
 
-// const s3BucketName = process.env.NEXT_PUBLIC_AWS_BUCKET_NAME
-// const s3BucketRegion = process.env.NEXT_PUBLIC_AWS_BUCKET_REGION
-// const s3AccessKey = process.env.NEXT_PUBLIC_AWS_ACCESS_KEY
-// const s3SecretKey = process.env.NEXT_PUBLIC_AWS_SECRET_KEY
+const s3BucketName = process.env.NEXT_PUBLIC_AWS_BUCKET_NAME
+const s3BucketRegion = process.env.NEXT_PUBLIC_AWS_BUCKET_REGION
+const s3AccessKey = process.env.NEXT_PUBLIC_AWS_ACCESS_KEY
+const s3SecretKey = process.env.NEXT_PUBLIC_AWS_SECRET_KEY
 
 const s3 = new S3Client({
   credentials: {
@@ -42,24 +44,19 @@ const s3 = new S3Client({
 })
 
 const config = sanityClient({
-  projectId: 'ugh5vm1v',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: 'production',
   apiVersion: '2021-03-25',
-  token:
-    'skVSEOqGcxVgsIPK9zjcltFDqU235c1ToyzTJauO9qrZxrJoUChMB7QVfo5RvhZyPInSRYMZxbOxcmctZMCDxyaNkSSkDfVA1OGYMLDQy40CVnea1AMlmKAdxgk7laYgxu2DVuMdZLq3kf2px9yBKWoXfXgmGGyuiFEAUAAWkNkKM42MTyzS',
+  token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
   useCdn: false,
   ignoreBrowserTokenWarning: true,
 })
 
-// import sanityClient from '@sanity/client'
-
 const storage = multer.memoryStorage()
 const upload = multer({ storage: storage })
 
-// const sanity = new sanityClient()
 
 app.get('/api/getS3Image', async (req, res) => {
-  // console.log('req.params', req.query.filename)
   const params = {
     Bucket: s3BucketName,
     Key: req.query.filename,
@@ -71,9 +68,9 @@ app.get('/api/getS3Image', async (req, res) => {
 })
 
 app.post('/api/saveS3Image', upload.single('profile'), async (req, res) => {
+  console.log(process.env.NEXT_PUBLIC_AWS_BUCKET_REGION)
   const address = req.body.userAddress
   console.log(req.file.buffer)
-  // const imageName = randomImageName() + '-' + req.file.originalname
   const imageName = 'profileImage-' + address
 
   const params = {
@@ -86,21 +83,11 @@ app.post('/api/saveS3Image', upload.single('profile'), async (req, res) => {
   const command = new PutObjectCommand(params)
 
   await s3.send(command)
-
-  // //saving image name in database
-  // await config
-  //   .patch(address)
-  //   .set({
-  //     profileImage: imageName,
-  //   })
-  //   .commit()
-
   res.send({})
 })
 
 app.post('/api/saveS3Banner', upload.single('banner'), async (req, res) => {
   const address = req.body.userAddress
-  // const imageName = randomImageName() + '-' + req.file.originalname
   const imageName = 'bannerImage-' + address
 
   const params = {
@@ -113,15 +100,6 @@ app.post('/api/saveS3Banner', upload.single('banner'), async (req, res) => {
   const command = new PutObjectCommand(params)
 
   await s3.send(command)
-
-  //saving image name in database
-  // await config
-  //   .patch(address)
-  //   .set({
-  //     bannerImage: imageName,
-  //   })
-  //   .commit()
-
   res.send({})
 })
 
