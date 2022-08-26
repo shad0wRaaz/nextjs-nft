08487cf3b48a72c9bb50ac4012c75fd9dd0a12d1
shad0wRaaz <007raaz.k@gmail.com>
2022-08-26 08:57:57 +0100

    Fix: Layout of Report Activities

--- components/nft/ReportActivity.js
+++ components/nft/ReportActivity.js
@@ -1,7 +1,13 @@
 import React, { useState } from 'react'
+import toast from 'react-hot-toast'
 import { BiChevronUp } from 'react-icons/bi'
 import { TiWarningOutline } from 'react-icons/ti'
+import { useQuery } from 'react-query'
 import { useThemeContext } from '../../contexts/ThemeContext'
+import { getReportActivities } from '../../fetchers/SanityFetchers'
+import moment from 'moment'
+import { RiTimerLine } from 'react-icons/ri'
+
 
 const style = {
     wrapper: `w-full mt-8 border rounded-xl overflow-hidden`,
@@ -9,11 +15,31 @@ const style = {
     titleLeft: `flex-1 flex items-center text-md font-bold`,
     titleIcon: `text-2xl mr-2`,
     titleRight: `text-xl transition`,
+    reportWrapper: 'p-4 px-6 flex flex-col gap-3 max-h-96 overflow-y-scroll',
+}
+
+const errorToastStyle = {
+    style: { background: '#ef4444', padding: '16px', color: '#fff' },
+    iconTheme: { primary: '#ffffff', secondary: '#ef4444' },
 }
 
 const ReportActivity = ({ collectionAddress, selectedNft }) => {
     const [toggle, setToggle] = useState(false)
     const { dark } = useThemeContext()
+    const { data:reportActivities, status } = useQuery(
+        ['reportactivities', collectionAddress],
+        getReportActivities(selectedNft?.metadata.id.toString()),
+        {
+            enabled: Boolean(selectedNft?.metadata.id),
+            onError: (error) => {
+                console.log(error)
+                toast.error('Error fetching report activities', errorToastStyle)
+            },
+            onSuccess: (res) => {
+                console.log(res)
+            }            
+        }
+    )
 
   return (
     <div
@@ -35,7 +61,7 @@ const ReportActivity = ({ collectionAddress, selectedNft }) => {
           <span className={style.titleIcon}>
             <TiWarningOutline />
           </span>
-          Report Activity
+          Report Activity {status == "success" && (`(${reportActivities.length})`)}
         </div>
         <div className={style.titleRight}>
           <BiChevronUp
@@ -48,7 +74,22 @@ const ReportActivity = ({ collectionAddress, selectedNft }) => {
         </div>
       </div>
       {!toggle && (
-        <div>Report data</div>
+        <div className={style.reportWrapper}>
+            {status == "success" && reportActivities.length == 0 && (<div className="text-center">No Reported Activities</div>)}
+            {status == "success" && reportActivities.length > 0 && reportActivities?.map((report, index) => (
+                <div className={`flex flex-row gap-3 py-2 border border-t-0 border-l-0 border-r-0 ${dark ? 'border-b-sky-700/30' : 'border-b-neutral-200'}`}>
+                    <div className="text-sm py-1">{index + 1}.</div>
+                    <div className="flex-grow">
+                        <div class="flex justify-between">
+                            <p className="rounded-lg py-1 text-sm px-3 w-fit bg-red-100 border-red-200 text-red-500">{report.eventTitle}</p>
+                            <p className="py-1 px-3 text-sm text-neutral-400"><RiTimerLine className="inline-block" /> <span className="inline-block">{moment(report._createdAt).fromNow()}</span></p>
+                        </div>
+                        <p className="pb-3 px-3 text-sm">{report.description}</p>
+                        
+                    </div>
+                </div>
+            ))}
+        </div>
       )}
     </div>
   )
