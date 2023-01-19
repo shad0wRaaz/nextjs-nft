4bac22c07c46a03caa4428a114a8ce78a1470bb9
shad0wRaaz <007raaz.k@gmail.com>
2023-01-19 13:08:59 +0000

    Add: Function to edit collection metadata and payment settings

--- mutators/Web3Mutators.js
+++ mutators/Web3Mutators.js
@@ -2,13 +2,25 @@ import { ThirdwebSDK } from '@thirdweb-dev/sdk'
 
 export const deployCollection = () => async () => {}
 
-export const updateCollectionMetaData = async ({newCollectionData, rpcUrl}) => {
-    //  console.log(newCollectionData)
-    // const sdk = new ThirdwebSDK('mumbai')
-    // const contract = await sdk.getContract(newCollectionData.contractAddress)
-    // console.log('metadata', await contract.metadata.get())
-    // await contract.metadata.set({
-    //     name: newCollectionData.name,
-    //     description: newCollectionData.description
-    // });
+export const updateCollectionMetaData = async (newCollectionData, signer, profile) => {
+
+    const sdk = new ThirdwebSDK(signer)
+    const contract = await sdk.getContract(newCollectionData.contractAddress, "nft-collection");
+
+    await contract.metadata.update({
+        name: newCollectionData.name,
+        description: newCollectionData.description,
+        image: profile,
+    });
+}
+
+export const updatePayment = async (collection, signer, basisPoints, recipient) => {
+    
+    const sdk = new ThirdwebSDK(signer);
+    const contract = await sdk.getContract(collection[0].contractAddress, "nft-collection");
+
+    await contract.royalties.setDefaultRoyaltyInfo({
+        seller_fee_basis_points: basisPoints * 100,
+        fee_recipient: recipient
+    })
 }
