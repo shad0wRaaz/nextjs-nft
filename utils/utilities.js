7890185e963d99743cb29ea99de7cde6dbc87a1e
shad0wRaaz <007raaz.k@gmail.com>
2023-03-19 10:44:40 +0000

    Add: Utilities functions

---
+++ utils/utilities.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export const checkValidEmail = (email) => {
+    //check email pattern first
+    const regex = new RegExp(
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+      )
+      const testemail = regex.test(email);
+      if(!testemail) {
+        return false;
+      }
+    return true
+}
+
+export const checkValidURL = (URL) => {
+    const regex = new RegExp(
+        '(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?'
+      )
+      return regex.test(URL)
+}
+
+export const generateRandomCode = () => {
+    const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const codeLength = 16;
+
+    let code = '';
+    const charactersLength = characters.length;
+    for ( let i = 0; i < codeLength; i++ ) {
+        code += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+
+    return code;
+
+}
