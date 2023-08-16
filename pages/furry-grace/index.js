267b12c1125bfc47613a77674e274d33fa877af9
shad0wRaaz <007raaz.k@gmail.com>
2023-08-16 18:50:02 +0100

    Fix: Hashtags in SEO changed

--- pages/furry-grace/index.js
+++ pages/furry-grace/index.js
@@ -10,18 +10,16 @@ import Videos from './components/Videos'
 import Gallery from './components/Gallery'
 import Roadmap from './components/Roadmap'
 import Community from './components/Community'
-import HelmetMetaData from '../../components/HelmetMetaData'
-import socialcoverimage from './assets/images/socialcoverimage.webp'
 
 const index = () => {
   const [showMenu, setShowMenu] = useState(false);
   const dataForSEO = {
-    title: 'Majestic Visions',
-    description: "THE TIME IS NOW; break free from the norm and take control of your future. On June 30th 2023, the world's first-ever referral and recurring income opportunity is launching with the Rewarding Renditions Nuva NFT collections! Take action today for paydays from the 30th of June; all payments will be in MATIC. This is your time, your opportunity to get in on the ground floor and take advantage of the optimum timing. It's simple; you only need to register your wallet on the Nuva NFT site and ensure you have MATIC to purchase your own NFTs and enjoy the benefits.  All of this without the need for registration or KYC! Simply connect with like-minded individuals and influencers since this is just the start. The MATIC program is merely the beginning, as you will be able to earn with the same network with four different programs! The time is now! Don't wait, don't hesitate, take advantage of this enormous opportunity to change your life. Join the revolution, register today, and get ready to reap the benefits of Referral and Recurring Income with Rewarding Renditions Nuva NFT collections –And yes, it's a DeFi. And yes, it's ONLY on nuvanft.io.  Launching on 30th June...This is YOUR time. Are you ready?",
+    title: 'Mushroom Kingdom',
+    description: "THE TIME IS NOW; break free from the norm and take control of your future. On August 1st, 2023, the world's first-ever referral and recurring income opportunity is launching with the Mushroom Kingdom Nuva NFT collections! Take action today for paydays from the 1st of august; all payments will be in ETH. This is your time, your opportunity to get in on the ground floor and take advantage of the optimum timing. It's simple; you only need to register your wallet on the Nuva NFT site and ensure you have BNB to purchase your own NFTs and enjoy the benefits.  All of this without the need for registration or KYC! Simply connect with like-minded individuals and influencers since this is just the start. The ETH program is merely the beginning, as you will be able to earn with the same network with four different programs! The time is now! Don't wait, don't hesitate, take advantage of this enormous opportunity to change your life. Join the revolution, register today, and get ready to reap the benefits of Referral and Recurring Income with Mushroom Kingdom Nuva NFT collections –And yes, it's a DeFi. And yes, it's ONLY on nuvanft.io.  Launching on 1st of August...This is YOUR time. Are you ready?",
     hashtag: "#nft #nfts #nftgame #nftart #nftcommunity #nftcollector #nftartist #nftgaming #nftcollection #nftcollectible #referral #4WaystoEarn #DirectReferrals #IndirectReferrals #RoyaltyShare #TransactionFee #Airdrop #ETH #NuvaTokenAirdrop #NuvaToken #SharedPlatformFee #Unilevel #NuvaNFT #MetaNuva #Ecosystem #NuvaGame #NuvaMarket #NuvaWallet #NuvaSwap #NuvaExchange #NuvaPay #NuvaFoundation #NuvaLean #NuvaCompare #NuvaFaucet #Ethereum #Binance #BinanceSmartChain #Polygon #Avalanche #contemporaryart #illustration #cryptocurrency #nftdrop #artwork #eth #nftart #digitalart #nftcollectibles #blockchain #defi #design #nftcollectors #crypto #artoftheday #artist #cryptoart #nftcollector #cryptoartist #nftcommunity #nftartist #btc #ethereum #art #love #drawing #raredigitalart #cryptotrading #nftartwork #music #nftartists #digital #cardano #abstractart #artcollector #digitalartist #modernart #artgallery #cryptonews #nftartgallery #nftcollection #abstract #painting #pixelart #photography #animation #artistsoninstagram #animation #digital #modernart #abstract #render #crypto #blockchain #digitalart #abstractart #artcollector #digitalartist #artoftheday #artwork #contemporaryart #painting #cryptocurrency #artgallery #eth #crypto #cryptonews #cryptotrading #cryptocurrencies #blockchain #render #binance #surreal #digitalartist #ethereum #defi",
     quote : '',
-    currentUrl: 'https://nuvanft.io/majestic-visions',
-    image: 'https://nuvanft.io/assets/socialcoverimage.webp',
+    currentUrl: 'https://nuvanft.io/mushroom-kingdom',
+    image: 'https://nuvanft.io/assets/mushroomkingdom.webp',
   }
   return (
     <>
@@ -32,7 +30,12 @@ const index = () => {
         image={dataForSEO.image}
         currentUrl={dataForSEO.currentUrl}
       />
-      <main className="majesticvisions">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link href="https://fonts.googleapis.com/css2?family=Henny+Penny&family=Oregano&display=swap" rel="stylesheet"/>
+      </Head>
+      <main className="rewardingrenditions">
           <Header showMenu={showMenu} setShowMenu={setShowMenu} />
           <Hero setShowMenu={setShowMenu}/>
           <Gallery setShowMenu={setShowMenu}/>
